449664417cb31673b63ab9be477704741d5b13ac
Zilin Chen <144270564+zilin-chen-1@users.noreply.github.com>
2025-01-13 14:00:15 +0000

    main

--- src/scenes/Game.ts
+++ src/scenes/Game.ts
@@ -28,7 +28,7 @@ export class Game extends Scene
         this.camera.setBackgroundColor(0x00ff00);
 
         this.background = this.add.image(0, 0, 'floor');
-        this.background.setDisplaySize(1170, (2232 / 3) * 2); // Covers the top half
+        this.background.setDisplaySize(1170, 2232 / 2);
 
         this.msg_text = this.add.text(512, 384, '', {
             fontFamily: 'Arial Black', fontSize: 38, color: '#ffffff',
